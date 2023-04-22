32bb3f935b650cd3871ba931f04dfda0324f6676
Andrew Combs <andrewcombs13@gmail.com>
2023-04-22 01:01:25 -0400

    I'm a dummy
    I changed the physical pin for comms and forgot to update the config lol.

--- keyboards/my4x5dactylmanuform/config.h
+++ keyboards/my4x5dactylmanuform/config.h
@@ -20,5 +20,5 @@
 //#define NO_ACTION_ONESHOT
 
 // #define SERIAL_PIO_USE_PIO1 // Force the usage of PIO1 peripheral, by default the Serial implementation uses the PIO0 peripheral
-#define SERIAL_USART_TX_PIN GP16 // Can't be defined in info.json b/c RP2040 doesn't support soft_serial_pin, and info.json doesn't allow usart_tx_pin
+#define SERIAL_USART_TX_PIN GP28 // Can't be defined in info.json b/c RP2040 doesn't support soft_serial_pin, and info.json doesn't allow usart_tx_pin
 // #define PAL_USE_CALLBACKS TRUE // Doesn't work? https://www.reddit.com/r/olkb/comments/12f2ywv/split_keyboard_with_stm32f401ccu6/
