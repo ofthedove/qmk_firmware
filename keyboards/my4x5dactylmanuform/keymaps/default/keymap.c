72bec2f7137de9fcd7c63104338ad66dee02cd52
Andrew Combs <andrewcombs13@gmail.com>
2023-04-28 11:13:35 -0400

    Refactor keymap
    I think the reason my keymap had to be in matrix order is because I put the key definitions in info.json into matrix order. Duh. I rearranged the json file to be in logical order, and also rearranged the keymap file to be in logical order and column formatted in a way that should make it a lot easier to update. I confirmed it compiles but I haven't actually tested it yet.

--- keyboards/my4x5dactylmanuform/keymaps/default/keymap.c
+++ keyboards/my4x5dactylmanuform/keymaps/default/keymap.c
@@ -15,41 +15,45 @@
 #define KC_MB1 KC_MS_BTN1
 #define KC_MB2 KC_MS_BTN2
 
+/* Blank Map for future copy-pasting
+      _______,    _______,    _______,    _______,    _______,          _______,    _______,     _______,   _______,    _______,
+      _______,    _______,    _______,    _______,    _______,          _______,    _______,     _______,   _______,    _______,
+      _______,    _______,    _______,    _______,    _______,          _______,    _______,     _______,   _______,    _______,
+                  _______,    _______,                                                           _______,   _______,
+                                          _______,    _______,          _______,    _______,
+                                          _______,    _______,          _______,    _______,
+                                          _______,    _______,          _______,    _______,
+*/
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Adapted from handwired/dactyl_manuform/4x5/keymaps/default
    [0] = LAYOUT_ortho_5x4(
-      KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,       KC_HOME,
-      KC_A,  KC_S,    KC_D,    KC_F,    KC_G,       KC_GRV,
-      KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,       MO(1),
-             KC_LBRC, KC_RBRC, SFT_ESC, CTL_BSPC,   KC_TAB,
-      
-      SFT_ENT, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-      KC_LGUI, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-      MO(2),   KC_N,    KC_M,    KC_COMM, KC_DOT,
-      KC_DEL,  ALT_SPC, SFT_ENT, KC_MINS, KC_EQL,  KC_QUOT
+      KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,             KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,
+      KC_A,       KC_S,       KC_D,       KC_F,       KC_G,             KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,
+      KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,             KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_QUOT,
+                  KC_LBRC,    KC_RBRC,                                                          KC_MINS,    KC_EQL,
+                                          SFT_ESC,    CTL_BSPC,         ALT_SPC,    SFT_ENT,
+                                          KC_TAB,     KC_HOME,          KC_END,     KC_DEL,
+                                          MO(1),      KC_GRV,           KC_LGUI,    MO(2)
    ),
 
    [1] = LAYOUT_ortho_5x4(
-      _______, KC_MB2,  KC_MU,   KC_MB1,  _______, _______,
-      _______, KC_ML,   KC_MD,   KC_MR,   _______, _______,
-      _______, _______, _______, _______, _______, _______,
-               _______, _______, _______, _______, _______,
-
-      _______, KC_VOLU, _______, KC_UP,   _______, KC_PGUP,
-      _______, KC_MUTE, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,
-      _______, KC_VOLD, KC_SLSH, KC_BSLS, KC_QUES,
-      _______, _______, _______, KC_MB1,  KC_MB2,  KC_PIPE
+      _______,    KC_MB2,     KC_MU,      KC_MB1,     _______,          KC_VOLU,    _______,    KC_UP,      _______,    KC_PGUP,
+      _______,    KC_ML,      KC_MD,      KC_MR,      _______,          KC_MUTE,    KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_PGDN,
+      _______,    _______,    _______,    _______,    _______,          KC_VOLD,    KC_SLSH,    KC_BSLS,    KC_QUES,    KC_PIPE,
+                  _______,    _______,                                                          KC_MB1,     KC_MB2,
+                                          _______,    _______,          _______,    _______,
+                                          _______,    _______,          _______,    _______,
+                                          _______,    _______,          _______,    _______
    ),
 
    [2] = LAYOUT_ortho_5x4(
-      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,
-      KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,
-      KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______,
-               KC_F11,  KC_F12,  _______, _______, _______,
-
-      _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-      _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-      _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,
-      _______, _______, _______, _______, _______, KC_RPRN
+      KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,            KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,
+      KC_1,       KC_2,       KC_3,       KC_4,       KC_5,             KC_6,       KC_7,       KC_8,       KC_9,       KC_0,
+      KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,          KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LPRN,    KC_RPRN,
+                  KC_F11,     KC_F12,                                                           _______,   _______,
+                                          _______,    _______,          _______,    _______,
+                                          _______,    _______,          _______,    _______,
+                                          _______,    _______,          _______,    _______
    )
 };
